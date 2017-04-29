7de46611ac965056cb1b9b494c38d23f2a7937ce
JingZhongzj <jz2748@columbia.edu>
2017-04-29 14:05:56 -0400

    Added explosion effect on the buttons

--- app/src/main/java/com/example/brian/mousecatelephant/HomeActivity.java
+++ app/src/main/java/com/example/brian/mousecatelephant/HomeActivity.java
@@ -11,19 +11,25 @@ import android.graphics.Color;
 import android.graphics.Shader.TileMode;
 import android.graphics.LinearGradient;
 import android.widget.TextView;
-
-
-
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.List;
+import android.view.ViewGroup;
+import android.view.View;
+import tyrantgit.explosionfield.ExplosionField;
+
+
 
 public class HomeActivity extends AppCompatActivity {
     Button exitButton, playEasyButton, playHardButton, rulesButton, multiplayButton;
     TextView text;
+    private ExplosionField mExplosionField;
     DatabaseHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
 
         db = new DatabaseHandler(this);
         db.deleteAll();
@@ -65,6 +71,8 @@ public class HomeActivity extends AppCompatActivity {
         text = (TextView) findViewById(R.id.Mqtext);
         text.bringToFront();
 
+        mExplosionField = ExplosionField.attach2Window(this);
+
         int[] color = {Color.MAGENTA,Color.CYAN};
         float[] position = {0, 1};
         TileMode tile_mode = TileMode.REPEAT;
@@ -76,40 +84,120 @@ public class HomeActivity extends AppCompatActivity {
     }
     private class MyOnClickListener implements View.OnClickListener{
         @Override
-        public void onClick(View v){
+        public void onClick(final View v){
             switch(v.getId()){
                 case R.id.playButton:
-                    startPlay("EASY");
+                    addListener(findViewById(R.id.playButton));
+                    mExplosionField.explode(v);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            reset(findViewById(R.id.playButton)); // Reset the view after explosion
+                            startPlay("EASY");
+                        }
+                    }, 1200);
                     break;
+
                 case R.id.playButton2:
-                    startPlay("HARD");
+                    addListener(findViewById(R.id.playButton2));
+                    mExplosionField.explode(v);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            reset(findViewById(R.id.playButton2)); // Reset the view after explosion
+                            startPlay("HARD");
+                        }
+                    }, 1200);
                     break;
+
                 case R.id.playButton3:
-                    startMultiplayer();
+                    addListener(findViewById(R.id.playButton3));
+                    mExplosionField.explode(v);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            reset(findViewById(R.id.playButton3)); // Reset the view after explosion
+                            startMultiplayer();
+                        }
+                    }, 1200);
                     break;
+
                 case R.id.exitButton:
-                    finish();
+                    addListener(findViewById(R.id.exitButton));
+                    mExplosionField.explode(v);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            reset(findViewById(R.id.exitButton)); // Reset the view after explosion
+                            finish();
+                        }
+                    }, 1200);
                     break;
+
                 case R.id.rulesButton:
-                    showRules();
+                    addListener(findViewById(R.id.rulesButton));
+                    mExplosionField.explode(v);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            reset(findViewById(R.id.rulesButton)); // Reset the view after explosion
+                            showRules();
+                        }
+                    }, 1200);
                     break;
+
                 default:
                     break;
             }
         }
     }
+
     public void startPlay(String mode) {
         //Intent intent = new Intent(this, MainActivity.class);
         Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra("game_mode", mode);
         startActivity(intent);
     }
+
     public void startMultiplayer(){
         Intent intent = new Intent(this, MultiplayActivity.class);
         startActivity(intent);
     }
+
     public void showRules() {
         Intent intent = new Intent(this, RulesActivity.class);
         startActivity(intent);
     }
+
+    private void addListener(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                addListener(parent.getChildAt(i));
+            }
+        } else {
+            root.setClickable(true);
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExplosionField.explode(v);
+                    v.setOnClickListener(null);
+                }
+            });
+        }
+    }
+
+    private void reset(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                reset(parent.getChildAt(i));
+            }
+        } else {
+            root.setScaleX(1);
+            root.setScaleY(1);
+            root.setAlpha(1);
+        }
+    }
+
 }
--- app/src/main/java/com/example/brian/mousecatelephant/LoginActivity.java
+++ app/src/main/java/com/example/brian/mousecatelephant/LoginActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import tyrantgit.explosionfield.ExplosionField;
+import android.view.View;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     String userInput, passInput, gameMode;
     TextView loginMsg;
     DatabaseHandler db;
+    private ExplosionField mExplosionField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +45,14 @@ public class LoginActivity extends AppCompatActivity {
 
         db = new DatabaseHandler(this);
 
+        mExplosionField = ExplosionField.attach2Window(this);
+
         gameMode = getIntent().getStringExtra("game_mode");
     }
 
     private class MyOnClickListener implements View.OnClickListener {
         @Override
-        public void onClick(View v) {
+        public void onClick(final View v) {
             Log.d("SQLite: ", "Reading all contacts..");
             /*List<User> users = db.getAllUsers();
 
@@ -59,9 +66,23 @@ public class LoginActivity extends AppCompatActivity {
             userInput = usernameField.getText().toString();
             passInput = passwordField.getText().toString();
             User user;
+
+
             try {
                 user = db.getUser(userInput);
-                if (user.getPassword().equals(passInput)) startPlay(gameMode, user.getName());
+                final View tmp = v;
+                addListener(v);
+                mExplosionField.explode(v);
+                if (user.getPassword().equals(passInput)) {
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                             // Reset the view after explosion
+                            reset(tmp);
+                            startPlay(gameMode, db.getUser(userInput).getName());
+                        }
+                    }, 1200);
+                }
                 else loginMsg.setText("Invalid username or password");
             }
             catch (Exception e){
@@ -73,10 +94,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+
     public void startPlay(String mode, String user){
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("game_mode", mode);
         intent.putExtra("user_name", user);
         startActivity(intent);
+    }
+
+    private void addListener(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                addListener(parent.getChildAt(i));
+            }
+        } else {
+            root.setClickable(true);
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExplosionField.explode(v);
+                    v.setOnClickListener(null);
+                }
+            });
+        }
+    }
+
+    private void reset(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                reset(parent.getChildAt(i));
+            }
+        } else {
+            root.setScaleX(1);
+            root.setScaleY(1);
+            root.setAlpha(1);
+        }
     }
 }
