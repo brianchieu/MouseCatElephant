72ba6755cbd9771b19b32462807273aad9576119
JingZhongzj <jz2748@columbia.edu>
2017-04-30 00:30:55 -0400

    Modified the UI

--- app/src/main/java/com/example/brian/mousecatelephant/LoginActivity.java
+++ app/src/main/java/com/example/brian/mousecatelephant/LoginActivity.java
@@ -11,28 +11,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import tyrantgit.explosionfield.ExplosionField;
-import android.view.View;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import com.sdsmdg.tastytoast.TastyToast;
+import com.yalantis.phoenix.PullToRefreshView;
+
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
+
 
 
 public class LoginActivity extends AppCompatActivity {
     EditText usernameField, passwordField;
-    Button submitButton;
+    Button submitButton, registerButton;
     String userInput, passInput, gameMode;
     TextView loginMsg;
     DatabaseHandler db;
+    private PullToRefreshView mPullToRefreshView;
     private ExplosionField mExplosionField;
+    private BoomMenuButton bmb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mPullToRefreshView = (PullToRefreshView) findViewById(R.id.pull_to_refresh);
+        mPullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mPullToRefreshView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPullToRefreshView.setRefreshing(false);
+                    }
+                }, 300);
+            }
+        });
 
         submitButton = (Button) findViewById(R.id.SubmitButton);
+        registerButton = (Button) findViewById(R.id.RegisterButton);
         MyOnClickListener myOnClickListener = new MyOnClickListener();
 
 
@@ -44,6 +65,9 @@ public class LoginActivity extends AppCompatActivity {
         submitButton.setOnClickListener(myOnClickListener);
         submitButton.bringToFront();
 
+        registerButton.setOnClickListener(myOnClickListener);
+        registerButton.bringToFront();
+
         loginMsg = (TextView) findViewById(R.id.loginMessage);
         loginMsg.bringToFront();
 
@@ -52,114 +76,136 @@ public class LoginActivity extends AppCompatActivity {
         mExplosionField = ExplosionField.attach2Window(this);
 
         gameMode = getIntent().getStringExtra("game_mode");
+
+        bmb = (BoomMenuButton) findViewById(R.id.bmb);
+        assert bmb != null;
+        bmb.setButtonEnum(ButtonEnum.TextInsideCircle);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_1);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.SC_1);
+        bmb.addBuilder(BuilderManager.getTextInsideCircleButtonBuilder());
     }
 
     private class MyOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.SubmitButton:
 
-            Log.d("SQLite: ", "Reading all contacts..");
-            /*List<User> users = db.getAllUsers();
+                    //Log.d("SQLite: ", "Reading all contacts..");
+                    /*List<User> users = db.getAllUsers();
 
-            for (User cn : users) {
-                String log = "Name: " + cn.getName() +
-                        ", Pass: " + cn.getPassword();
-                // Writing Contacts to log
-                Log.d("SQLite: ", log);
-            }*/
+                    for (User cn : users) {
+                        String log = "Name: " + cn.getName() +
+                                ", Pass: " + cn.getPassword();
+                        // Writing Contacts to log
+                        Log.d("SQLite: ", log);
+                    }*/
 
-            userInput = usernameField.getText().toString();
-            passInput = passwordField.getText().toString();
-            User user;
+                    userInput = usernameField.getText().toString();
+                    passInput = passwordField.getText().toString();
+                    User user;
 
-            try {
-                user = db.getUser(userInput);
-                if (user.getPassword().equals(passInput)) {
-                    final View tmp = v;
-                    addListener(tmp);
-                    mExplosionField.explode(v);
-                    showSuccessToast(v);
-                    new Timer().schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            reset(tmp);// Reset the view after explosion
-                            startPlay(gameMode, db.getUser(userInput).getName());
+                    try {
+                        user = db.getUser(userInput);
+                        if (user.getPassword().equals(passInput)) {
+                            final View tmp = v;
+                            addListener(tmp);
+                            mExplosionField.explode(v);
+                            showSuccessToast(v);
+                            new Timer().schedule(new TimerTask() {
+                                @Override
+                                public void run() {
+                                    reset(tmp);// Reset the view after explosion
+                                    startPlay(gameMode, db.getUser(userInput).getName());
+                                }
+                            }, 1200);
+                        } else {
+                            showErrorToast(v);
                         }
-                    }, 1200);
-                }
+                    } catch (Exception e) {
+                        if (passInput.equals(null) || passInput.equals("")) showErrorToast(v);
+                        else {
+                            db.addUser(new User(userInput, passInput));
+                            reset(v);
+                            showWarningToast(v);
+                        }
+                    }
+                    break;
 
-                else {
-                    showErrorToast(v);
-                }
-            }
-            catch (Exception e){
-                if (passInput.equals(null) || passInput.equals("")) loginMsg.setText("Please enter a valid password for new user.");
-                else {
-                    db.addUser(new User(userInput, passInput));
-                    reset(v);
-                    showWarningToast(v);
-                }
+                case R.id.RegisterButton:
+                    startRegistration();
+                    break;
+
+                default:
+                    break;
             }
         }
     }
 
-    public void startPlay(String mode, String user){
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("game_mode", mode);
-        intent.putExtra("user_name", user);
-        startActivity(intent);
-    }
+        public void startPlay(String mode, String user) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("game_mode", mode);
+            intent.putExtra("user_name", user);
+            startActivity(intent);
+        }
 
-    private void addListener(View root) {
-        if (root instanceof ViewGroup) {
-            ViewGroup parent = (ViewGroup) root;
-            for (int i = 0; i < parent.getChildCount(); i++) {
-                addListener(parent.getChildAt(i));
-            }
-        } else {
-            root.setClickable(true);
-            root.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mExplosionField.explode(v);
-                    v.setOnClickListener(null);
+        private void addListener(View root) {
+            if (root instanceof ViewGroup) {
+                ViewGroup parent = (ViewGroup) root;
+                for (int i = 0; i < parent.getChildCount(); i++) {
+                    addListener(parent.getChildAt(i));
                 }
-            });
-        }
-    }
-
-    private void reset(View root) {
-        if (root instanceof ViewGroup) {
-            ViewGroup parent = (ViewGroup) root;
-            for (int i = 0; i < parent.getChildCount(); i++) {
-                reset(parent.getChildAt(i));
+            } else {
+                root.setClickable(true);
+                root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mExplosionField.explode(v);
+                        v.setOnClickListener(null);
+                    }
+                });
             }
-        } else {
-            root.setScaleX(1);
-            root.setScaleY(1);
-            root.setAlpha(1);
         }
-    }
 
-    public void showSuccessToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Let's go !", TastyToast.LENGTH_LONG,
-                TastyToast.SUCCESS);
-    }
 
-    public void showWarningToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Created new user ! \nSubmit again to login !", TastyToast.LENGTH_LONG,
-                TastyToast.WARNING);
-    }
+        private void reset(View root) {
+            if (root instanceof ViewGroup) {
+                ViewGroup parent = (ViewGroup) root;
+                for (int i = 0; i < parent.getChildCount(); i++) {
+                    reset(parent.getChildAt(i));
+                }
+            } else {
+                root.setScaleX(1);
+                root.setScaleY(1);
+                root.setAlpha(1);
+            }
+        }
 
-    public void showErrorToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Invalid username or password !", TastyToast.LENGTH_LONG,
-                TastyToast.ERROR);
-    }
+        public void showSuccessToast(View view) {
+            TastyToast.makeText(getApplicationContext(), "Let's go !", TastyToast.LENGTH_SHORT,
+                    TastyToast.SUCCESS);
+        }
 
-    @Override
-    public void onBackPressed() {
-        // Do Here what ever you want do on back press;
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-    }
+        public void showWarningToast(View view) {
+            TastyToast.makeText(getApplicationContext(), "Created new user ! \nSubmit again to login !", TastyToast.LENGTH_SHORT,
+                    TastyToast.WARNING);
+        }
+
+        public void showErrorToast(View view) {
+            TastyToast.makeText(getApplicationContext(), "Invalid username or password !", TastyToast.LENGTH_SHORT,
+                    TastyToast.ERROR);
+        }
+
+        public void startRegistration() {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onBackPressed() {
+            // Do Here what ever you want do on back press;
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
 }
+
